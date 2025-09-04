a8c968dd69295c859995e6baa626c4506f7e6538
vuvormartin2025 <vuvormartin@gmail.com>
2025-09-04 19:18:39 +0000

    Django admin integration upgrade

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+# Custom Admin configuration for the Book model
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Columns shown in the list view
+    list_display = ("title", "author", "publication_year")
+
+    # Sidebar filters
+    list_filter = ("author", "publication_year")
+
+    # Search functionality
+    search_fields = ("title", "author")
