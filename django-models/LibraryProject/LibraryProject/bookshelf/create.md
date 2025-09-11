9971791952c1fa4980689a546a766b8ae21a2339
vuvormartin2025 <vuvormartin@gmail.com>
2025-09-11 20:37:08 +0000

    Adding relationship_app project

---
+++ django-models/LibraryProject/LibraryProject/bookshelf/create.md
@@ -0,0 +1,5 @@
+from bookshelf.models import Book
+
+# Create
+book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
+book
---
+++ django-models/LibraryProject/LibraryProject/relationship_app/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class Author(models.Model):
+    """Represents a book author."""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """Represents a book written by an Author."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+
+
+class Library(models.Model):
+    """Represents a library that contains many books."""
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    """Each library has exactly one librarian."""
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(
+        Library,
+        on_delete=models.CASCADE,
+        related_name="librarian"
+    )
+
+    def __str__(self):
+        return f"{self.name} (Librarian of {self.library.name})"
