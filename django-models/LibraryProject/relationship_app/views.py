9ba6b36fe813a72d4751b50562b6aec07d3762c8
vuvormartin2025 <vuvormartin@gmail.com>
2025-09-12 21:01:27 +0000

    Adding views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Book, Library
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+# Class-based view: show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -20,3 +20,17 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/", views.list_books, name="list_books"),  # Function-based view
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # Class-based view
+]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("relationship_app.urls")),  # add this line
+]
