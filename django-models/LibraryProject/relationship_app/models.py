23355e23866b507a1d37b2ac3962b4c18d46cfee
vuvormartin2025 <vuvormartin@gmail.com>
2025-09-12 14:17:34 +0000

    relationship project

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,36 @@
+
+from django.db import models
+
+# Author has many Books (One-to-Many)
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# Book belongs to one Author
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
+
+
+# Library has many Books (Many-to-Many)
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    def __str__(self):
+        return self.name
+
+
+# Each Library has exactly one Librarian (One-to-One)
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __str__(self):
+        return f"{self.name} ({self.library.name})"
