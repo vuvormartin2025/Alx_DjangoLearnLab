d9132e6263b8d1c496a88f4e6be2c04bd08c3205
vuvormartin2025 <vuvormartin@gmail.com>
2025-09-13 22:26:50 +0000

    Edited file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -39,9 +40,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register_view, template_name="register"),
+    path("login/", views.login_view, template_name="login"),
+    path("logout/", views.logout_view, template_name="logout"),
 ]
 
 from django.contrib import admin
