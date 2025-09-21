135dbda06933c89956648905b3e530ca59b36eaa
vuvormartin2025 <vuvormartin@gmail.com>
2025-09-21 17:20:51 +0000

    Adding edited api_project file

---
+++ api_project/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.title
