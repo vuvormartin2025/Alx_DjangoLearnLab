ed596ae0eb69b406ccfb79b77c96a1fdfd7cb019
vuvormartin2025 <vuvormartin@gmail.com>
2025-10-04 22:33:30 +0000

    Adding Edited file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 # api/views.py
-
+from django_filters import rest_framework
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
