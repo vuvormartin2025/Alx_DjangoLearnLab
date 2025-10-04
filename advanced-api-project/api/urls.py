29c203b9b4e29d19a4a27f94f1c0bdf4f73131ef
vuvormartin2025 <vuvormartin@gmail.com>
2025-10-04 21:59:51 +0000

    adding updated file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # 👈 this line connects your api app routes
+]
+
 from django.urls import path
 from .views import (
     BookListView,
@@ -11,15 +19,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
-]
-
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # 👈 this line connects your api app routes
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),   
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),   
 ]
