ee0e0feca1a3cc3f3348e0360036cbeb5c71c67e
vuvormartin2025 <vuvormartin@gmail.com>
2025-10-11 14:24:00 +0000

    Adding edited file

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
